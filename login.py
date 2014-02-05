5b676444b1b405209ca1e0735eef45680c412d6f
Ishaan Kapri <ishaankapri11@gmail.com>
2014-02-06 03:10:33 +0530

    login done

--- login.py
+++ login.py
@@ -5,13 +5,11 @@ HOME_URL = 'http://www.codechef.com'
 LOGIN_URL = 'http://www.codechef.com/node?destination=node'
 UA = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'
 headers = {
-		'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
-		'Referer':'http://codechef.com'
-}
+		'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'		
+		}
 def login(username, password):
 	try:
-		proxies = {'http':'http://192.168.1.7:8080','https':'http://192.168.1.7:8080'}
-		r = requests.get(HOME_URL,headers=headers,proxies=proxies)
+		r = requests.get(HOME_URL,headers=headers)
 		if r.ok:
 			soup = BeautifulSoup(r.text)
 			form = soup.find('form',{'id':'user-login-form'})
@@ -26,10 +24,11 @@ def login(username, password):
 					'form_id':'user_login_block'
 			}
 			s = requests.Session()
+			headers['Referer'] = 'http://codechef.com'
 			cookies = None
 			url = LOGIN_URL
 			for i in range(6):
-				r = s.post(url,headers=headers,data=data,proxies=proxies,allow_redirects=False)
+				r = s.post(url,headers=headers,data=data,allow_redirects=False)
 				if r.status_code in [301,302]:
 					url = r.headers['Location']
 				elif r.status_code == 200:
