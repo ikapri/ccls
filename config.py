32583e29171e870c59e2260d7487888e4ba410d7
Ishaan <ishaankapri11@gmail.com>
2014-02-10 01:22:55 +0530

    submit function done

---
+++ config.py
@@ -0,0 +1,7 @@
+HOME_URL = 'http://www.codechef.com'
+LOGIN_URL = 'http://www.codechef.com/node?destination=node'
+SUBMIT_URL = 'http://www.codechef.com/%ssubmit/%s'
+LOGOUT_URL = 'http://www.codechef.com/logout'
+headers = {
+		'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'		
+		}
---
+++ util.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+from config import *
+
+def login(username, password):
+	try:
+		r = requests.get(HOME_URL,headers=headers)
+		if r.ok:
+			soup = BeautifulSoup(r.text)
+			form = soup.find('form',{'id':'user-login-form'})
+			form_build_elem = form.find('input',{'name':'form_build_id'})
+			form_build_id = form_build_elem.get('value')
+			data = {
+					'name':username,
+					'pass':password,
+					'submit.x':14,
+					'submit.y':9,
+					'form_build_id':form_build_id,
+					'form_id':'user_login_block'
+			}
+			s = requests.Session()
+			headers['Referer'] = 'http://www.codechef.com'
+			cookies = None
+			url = LOGIN_URL
+			for i in range(6):
+				r = s.post(url,headers=headers,data=data,allow_redirects=False)
+				if r.status_code in [301,302]:
+					url = r.headers['Location']
+				elif r.status_code == 200:
+					cookies = s.cookies
+					break
+			return cookies.get_dict()
+	except Exception as e:
+		print e
+
+def submit(cookies,pname,file,lang_code,contest=''):
+	if contest:
+		url = SUBMIT_URL % (contest+'/', pname)
+	else:
+		url = SUBMIT_URL % ('', pname)
+	try:
+		r = requests.get(url,headers=headers,cookies=cookies)
+		if r.ok:
+			soup = BeautifulSoup(r.text)
+			form = soup.find('form')
+			form_build_id = form.find('input',{'name':'form_build_id'}).get('value')
+			form_token = form.find('input',{'name':'form_token'}).get('value')
+			form_id = form.find('input',{'name':'form_id'}).get('value')
+			unique_id = form.find('input',{'name':'submission_unique_id'}).get('value')
+			title = form.find('input',{'name':'title'}).get('value')
+			problem_code = pname
+			submission_language = lang_code
+			data = {
+					'form_build_id':(None,form_build_id,None),
+					'form_token':(None,form_token,None),
+					'form_id':(None,form_id,None),
+					'submission_unique_id':(None,unique_id,None),
+					'problem_code':(None,problem_code,None),
+					'title':(None,title,None),
+					'body':(None,'',None),
+					'files[program_file]':(pname,open(file,'rb'),'application/octet-stream'),
+					'submission_language':(None,submission_language,None),
+					'changed':(None,'',None)
+			}
+			headers['Referer'] = url
+			r = requests.post(url,headers=headers,files=data,cookies=cookies)
+			if r.ok:
+				return r
+	except Exception as e:
+		print e
+	return False
+	
+def logout(cookies):
+	try:
+		r = requests.get(LOGOUT_URL,headers=headers,cookies=cookies)
+		return r.ok
+	except Exception as e:
+		print e
+		return False
+
+def get_submission_status(response):
+	pass
